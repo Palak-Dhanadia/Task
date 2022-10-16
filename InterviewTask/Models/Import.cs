cd2a3363f2c6ce6c1c3c5d55a51b05f56e829f8b
Palak Dhanadia <98673846+Palak-Dhanadia@users.noreply.github.com>
2022-10-16 21:36:06 +0530

    Add files via upload

---
+++ InterviewTask/Models/Import.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InterviewTask.Support;
+
+namespace InterviewTask.Models
+{
+    public class Import
+    {
+        public IFormFile FormFile { get; set; }
+        public IEnumerable<ConsignmentInformation> Records { get; set; }
+    }
+}
